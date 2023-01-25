bcfcc223b62f3fade6d376baaf1fe8237e71c053
+ <+>
2023-01-25 20:23:37 +0300

    Made changes to file

--- my-app/src/components/Creation.js
+++ my-app/src/components/Creation.js
@@ -1,9 +1,163 @@
-const creation = () => {
-  return(
-  <section className='hero'>
-             <h1>My Creation</h1>
-        </section>
-           
-  )
-}
-export default creation;
+import React, { useState, useEffect } from "react";
+const Creation = () => {
+  const [fetchData, setCard] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3700/recipes")
+      .then((res) => res.json())
+      .then((data) => setCard(data));
+  }, []);
+
+  //console.log(fetchData)
+
+  const [formData, setFormData] = useState({
+    image: "",
+    category: "",
+    ingredients: "",
+    directions: "",
+  });
+
+  const handleOnChange = (event) => {
+    formData[event.target.name] = event.target.value;
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+    //console.log(formData)
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setCard([...fetchData, formData]);
+
+    fetch("http://localhost:3700/recipes", {
+      method: "POST",
+      headers: { "content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => document.location.reload())
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  // console.log(fetchData)
+  return (
+    <section className="hero">
+      <h1>My Creation</h1>
+      <div
+        className="container-fluid"
+        style={{ marginTop: "20px", paddingLeft: "25px" }}
+      >
+        <form
+          onSubmit={handleSubmit}
+          className="row p-3 container-fluid"
+          style={{
+            backgroundColor: "whitesmoke",
+            borderRadius: "5px",
+            outlineColor: "whitesmoke",
+          }}
+        >
+          <div className="col-3">
+            <input
+              onChange={handleOnChange}
+              name="image"
+              value={formData.image}
+              className="form-control form-control-sm"
+              type="text"
+              placeholder="Image"
+              aria-label=".form-control-sm example"
+            />
+          </div>
+          <div className="col-3">
+            <textarea   onChange={handleOnChange}
+              name="ingredients"
+              value={formData.ingredients}
+              className="form-control form-control-sm"
+              type="text"
+              placeholder="ingredients"
+              aria-label=".form-control-sm example"
+              rows="3">
+                
+              </textarea>
+          </div>
+          <div className="col-3">
+            <select onChange={handleOnChange} name="category">
+              <option>beef</option>
+              <option>chicken</option>
+              <option>dessert</option>
+              <option>lamb</option>
+              <option>pasta</option>
+              <option>pork</option>
+              <option>seaFood</option>
+              <option>side</option>
+              <option>starter</option>
+              <option>vegan</option>
+              <option>vegeterian</option>
+              <option>breakfast</option>
+              <option>goat</option>
+              <option>miscellaneous</option>
+            </select>
+          </div>
+          <div className="col-3">
+            <textarea
+              onChange={handleOnChange}
+              value={formData.directions}
+              aria-label=".form-control-sm example"
+              placeholder="preparations"
+              name="directions"
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3">
+                 {/* {formData.directions} */}
+            </textarea>
+          </div>
+            
+          <div className="col-3 mx-auto p-3">
+            <button className="btn btn-sm btn-success mx-auto" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+<div className="container-fluid" style={{ border: "2px solid white",borderRadius:"10px",margin:"10px", display:"flex", gap:"20px", flexWrap:"wrap", padding:"100px"}}>
+      {fetchData.map((data, index) => {
+        return (
+          
+            <div key={index} className="card" style={{ width: "18rem" }}>
+              <img className="card-img-top" src={data.image} alt="Card" />
+              <div className="card-body">
+                <h5 className="card-title" style={{ color: "white" }}>
+                 <b> {data.category} </b>
+                </h5>
+                <h5
+                  className="card-title"
+                  style={{ color: "Black", textAlign: "center" }}
+                >
+                  <b>Ingredients</b>
+                </h5>
+                <p className="card-text">{data.ingredients}</p>
+              </div>
+
+              <div className="card-body">
+                <h5
+                  className="card-title"
+                  style={{ color: "Black", textAlign: "center" }}
+                >
+                  <b>Directions</b>
+                </h5>
+                <p className="card-text">{data.directions}</p>
+              </div>
+              <button className="btn btn-outline-secondary btn-sm">Delete</button>
+            </div>
+          // </div>
+        );
+      })}
+      </div>
+    </section>
+  );
+};
+export default Creation;
